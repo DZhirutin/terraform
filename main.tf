aff8cc305867994f08508ceea9adee38f7d5dd29
DZhirutin <dzhirutin@live.ru>
2021-07-02 19:37:59 +0300

    update terraform

--- main.tf
+++ main.tf
@@ -13,10 +13,12 @@ provider "google" {
   zone        = "europe-west1-b"
 }
 
+
 //Create instance build
 resource "google_compute_instance" "Build" {
   name         = "build"
   machine_type = "f1-micro"
+  tags = ["myssh"]
   boot_disk {
     initialize_params {
       image = "ubuntu-2004-focal-v20210623"
@@ -35,6 +37,7 @@ resource "google_compute_instance" "Build" {
 resource "google_compute_instance" "Prod" {
   name         = "prod"
   machine_type = "f1-micro"
+  tags = ["myssh","mytomcat"]
   boot_disk {
     initialize_params {
       image = "ubuntu-2004-focal-v20210623"
@@ -48,6 +51,37 @@ resource "google_compute_instance" "Prod" {
     }
   }
 }
+//Create firewall rule ssh
+resource "google_compute_firewall" "myssh" {
+  name    = "myssh"
+  network = "default"
+
+  allow {
+    protocol = "tcp"
+    ports    = ["22"]
+    
+  }
+
+  source_tags = ["myssh"]
+  source_ranges = ["149.62.52.67/32"]
+}
+//Create firewall rule 8080 tomcat
+resource "google_compute_firewall" "mytomcat" {
+  name    = "mytomcat"
+  network = "default"
+
+  allow {
+    protocol = "tcp"
+    ports    = ["8080","80","443"]
+    
+  }
+
+  source_tags = ["mytomcat"]
+  source_ranges = ["0.0.0.0/0"]
+}
+
+
+
 
 output "Public_IP_address-Build" {
   value = google_compute_instance.Build.network_interface.0.access_config.0.nat_ip
